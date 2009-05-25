648c108aa9e924a6d9d68b9bb61ee50a8b86bdba
David Fugere <github@surfmontreal.com>
2009-05-25 16:18:13 -0400

    generate all models

--- generators/flat_lady/flat_lady_generator.rb
+++ generators/flat_lady/flat_lady_generator.rb
@@ -9,19 +9,36 @@ class FlatLadyGenerator < Rails::Generator::Base
       m.directory File.join("test", "factories")
       m.file "factories.rb", "test/factories/flat_lady.rb"
       m.directory File.join("config")
-      m.file "import_mapping.yml", "config/import_mapping.yml"
       m.file "import.css", "public/stylesheets/import.css"
       unless  File.exists?('db/migrate/create_import_files.rb')
         m.migration_template( "migrations/create_import_files.rb",
                              'db/migrate',
                              :migration_file_name => "flat_lady_create_import_files")
       end
-
+      m.directory File.join("app", "controllers", "flat_lady")
+      m.directory File.join("app", "models" )
+      
+      m.directory File.join("app", "views", "flat_lady","import_files")
+      scaffold_views.each do |action|
+        m.template(
+          "views/flat_lady/import_files/#{action}.html.haml",
+          File.join('app/views', "flat_lady", "import_files" , "#{action}.html.haml")
+        )
+      end
+      m.file "views/layouts/import.html.haml", "app/views/layouts/import.html.haml"
+      m.template('model.rb', File.join('app/models', "import_file.rb"))
+      m.template('controller.rb', File.join('app/controllers', "flat_lady", "import_files_controller.rb"))
+    
       m.readme "README"
     end
   end
+  def rspec_views
+    %w[ index show new edit ]
+  end
 
- 
+  def scaffold_views
+    rspec_views + %w[ _form ]
+  end
  
 
 end
